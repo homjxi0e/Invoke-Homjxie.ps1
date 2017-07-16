22b2a76485d9ab48a4bd3ae3dd9a517f902666dd
Homjxie  ✔️ <jihadLkmaty218@users.noreply.github.com>
2017-07-16 07:46:14 +0200

    Add files via upload

---
+++ Invoke-Homjxie.ps1
@@ -0,0 +1,21 @@
+########################################################
+# ( Twitter )                                          #
+# https://www.Twitter.com/GihadAlkmaty                 #
+########################################################
+# ( Facebook )                                         #
+# https://www.facebook.com/GihadAlkmatySy              #
+########################################################
+# ( Github )                                           #
+# https://www.Github.com/jihadLkmaty218                #
+########################################################
+
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force #Bypass disable Script
+echo "-----------------"
+echo "Username"
+Read-Host "----------------->"
+sleep 1.33
+echo "------------------"
+echo " Password "
+Read-Host "--------------->"
+echo "-----------------"
+sleep 1.33
